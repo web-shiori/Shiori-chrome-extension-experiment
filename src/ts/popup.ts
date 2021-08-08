7877e0f7744380074a7bf67db1a2e74a49c7bf76
masatora <atarashi.masatora@gmail.com>
2021-08-08 22:17:17 +0900

    取得したメタデータをストレージに保存

--- src/ts/popup.ts
+++ src/ts/popup.ts
@@ -118,37 +118,62 @@ function getScrollPositionX(): number {
     return 0
 }
 
-function getScrollPositionY(): number {
-    chrome.tabs.getSelected(tab => {
-        // @ts-ignore
-        chrome.tabs.executeScript(tab.id, {
-            code: `
+function getScrollPositionY() {
+    return new Promise((resolve) => {
+        chrome.tabs.getSelected(tab => {
+            // @ts-ignore
+            chrome.tabs.executeScript(tab.id, {
+                code: `
                 String(Math.max(window.pageYOffset, document.documentElement.scrollTop, document.body.scrollTop));
             `
-        }, function (result) {
-            console.log("Y")
-            console.log(result[0])
-            console.log(typeof result[0])
-            const a: number = result[0]
-            console.log(a)
-            return a
+            },(result) => {
+                console.log(`getScrollPositionY: ${result[0]}`)
+                const positionY: number = Number(result[0])
+                resolve({ positionY })
+            })
+        })
+
+    })
+    // let a: number = 0
+    // // a = await unko()
+    // chrome.tabs.getSelected(tab => {
+    //     // @ts-ignore
+    //     chrome.tabs.executeScript(tab.id, {
+    //         code: `
+    //             String(Math.max(window.pageYOffset, document.documentElement.scrollTop, document.body.scrollTop));
+    //         `
+    //     }, async (result) => {
+    //         a = result[0]
+    //     })
+    // })
+    // console.log(`a: ${a}`)
+    // // blockTime(1000)
+    // return a
+}
+
+function getMetaFromSelectedTab() {
+    return new Promise((resolve) => {
+        chrome.tabs.getSelected(tab => {
+            console.log("getSelected")
+            const url = tab.url ?? "a"
+            const title = tab.title ?? ""
+            const height = tab.height ?? 0
+            const width = tab.width ?? 0
+            console.log(`tab.url: ${tab.url}`)
+            console.log(`url: ${url}`)
+            resolve({
+                url: url,
+                title,
+                height,
+                width
+            })
         })
     })
-    return 0
+
+
 }
 
-function blockTime(timeout: number) {
-    const startTime = Date.now();
-    // `timeout`ミリ秒経過するまで無限ループをする
-    while (true) {
-        const diffTime = Date.now() - startTime;
-        if (diffTime >= timeout) {
-            return; // 指定時間経過したら関数の実行を終了
-        }
-    }
-}
-
-function getMetaData(): MetaData {
+async function getMetaData() {
     let title = ""
     let url = ""
     let height = 0
@@ -157,24 +182,30 @@ function getMetaData(): MetaData {
     let positionY = 0
     let videoPlayBackPosition = 0
 
-    chrome.tabs.getSelected(tab => {
-        url = tab.url ?? "a"
-        title = tab.title ?? ""
-        height = tab.height ?? 0
-        width = tab.width ?? 0
-    })
-
-    // blockTime(1000)
+    // 実行される順番が最後。だから反映されてない。同期的に書かないといけない。。。
+    // 非同期処理むずい、、けど勉強になる
+    const a = await getMetaFromSelectedTab()
+    // @ts-ignore
+    title = a.title
+    // @ts-ignore
+    url = a.url
+    // @ts-ignore
+    height = a.height
+    // @ts-ignore
+    width = a.width
     positionX = getScrollPositionX()
-    // blockTime(1000)
-    positionY = getScrollPositionY()
-    // blockTime(1000)
+    console.log("getScrollPositionY")
+    console.log(getScrollPositionY())
+    // @ts-ignore
+    const pY = await getScrollPositionY()
+    // @ts-ignore
+    positionY = pY.positionY
     videoPlayBackPosition = getVideoPlayBackPosition()
-    // blockTime(1000)
     console.log("getMetaData")
     console.log(`title: ${title}`)
     console.log(`url: ${url}`)
     console.log(`py: ${positionY}`)
+    console.log("end getMetaData")
     let metaData: MetaData = {
         title,
         url,
@@ -190,10 +221,14 @@ function getMetaData(): MetaData {
 const StorageKey = 'mylist';
 const button_save = document.getElementById('button-save')
 button_save?.addEventListener('click', function () {
-    const content: MetaData = getMetaData()
-    console.log(`title: ${content.title}`)
-    setToStorage(StorageKey, content)
-    console.log(content.url)
+    getMetaData().then((result) => {
+        const content: MetaData = result
+        console.log("save button pressed")
+        console.log(`title: ${content.title}`)
+        setToStorage(StorageKey, content)
+        console.log(content.url)
+    })
+
 })
 
 const button_load = document.getElementById('button-load')
